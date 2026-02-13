eef70364a8c7eb7c48d2347d95f62086e2d21a5c
goodbyebada <goodbyebada@naver.com>
2026-02-13 14:03:39 +0900

    feat: 날씨 상세 페이지 구현 및 라우터 연결
    - WeatherDetailPage 구현 (메인 날씨 카드, 추가 정보, 시간별 예보)
    - /weather/:lat/:lon 경로를 WeatherDetailPage로 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryProvider } from "@app/providers/QueryProvider";
 import MainPage from "@pages/main/MainPage";
+import WeatherDetailPage from "@pages/weather-detail/WeatherDetailPage";
 import { TestPage } from "@pages/test/TestPage";
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/weather/:lat/:lon" element={<WeatherDetailPage />} />
           <Route path="/test" element={<TestPage />} />
-          <Route path="/weather/:lat/:lon" element={<TestPage />} />
         </Routes>
       </BrowserRouter>
     </QueryProvider>
