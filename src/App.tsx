9b21cfe0f3182cfbe58aac4a281e43a66dc61934
goodbyebada <goodbyebada@naver.com>
2026-02-13 14:23:39 +0900

    feat: 404 Not Found 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryProvider } from "@app/providers/QueryProvider";
 import MainPage from "@pages/main/MainPage";
 import WeatherDetailPage from "@pages/weather-detail/WeatherDetailPage";
+import NotFoundPage from "@pages/not-found/NotFoundPage";
 import { TestPage } from "@pages/test/TestPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/weather/:lat/:lon" element={<WeatherDetailPage />} />
           <Route path="/test" element={<TestPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </QueryProvider>
